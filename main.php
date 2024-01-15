197ba4749c4fc15edc7b952e8cb9fcaef13b3856
JOHIRRIHAN <jr7718301@gmail.com>
2024-01-16 00:48:20 +0600

    first commit

---
+++ main.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+$condition1 = false;
+$condition2 = true;
+$condition3 = true;
+
+if($condition1 && $condition2 && $condition3){
+    echo "Hello";
+}
+elseif($condition1 && $condition2){
+    echo "Hello no 1";
+}
+if($condition1){
+    echo "Hello no 2";
+}
+else{
+    echo "no 3"; 
+}
+*/
+
+//  ternary operator
+/*
+$n = 11;
+if($n % 2 == 0){
+    echo "$n is Even number";
+}else{
+    echo "$n is odd number";
+}
+echo "<br>";
+$results = ($n % 2 == 0) ? "even number" : "odd number";
+echo $results;
+*/
+
+// switch 
+/*
+$n = 12;
+$r = $n % 2;
+switch ($r){
+    case 0:
+        switch($n){
+            case $n >0:
+                echo "$n is a positive even number";
+                break;
+                case $n<0:
+                    echo "$n is a negative odd number";
+                    break;
+        }
+        break;
+    default:
+      switch($n){
+        case $n > 0:
+            echo "$n is a positive odd number";
+            break;
+        case $n< 0:
+            echo "$n is a negative number";
+            break;
+      }
+}
+
+*/
+
